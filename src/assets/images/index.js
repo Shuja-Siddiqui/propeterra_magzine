50feaef7ce6aeb3787a2102d3023c591537efa22
Shuja Ur Rehman <shuja0094@gmail.com>
2024-06-26 18:17:53 +0500

    slideshow

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,5 +2,16 @@ import companyLogo from "./pronewlogo.webp";
 import companyLogo2 from "./PROPETER (100 x 40 px).png";
 import googlePlay from "./google-play.png";
 import appStore from "./app-store-preview.png";
+import autoSlide1 from "./auto1.webp";
+import autoSlide2 from "./auto2.jpg";
+import autoSlide3 from "./auto3.jpg";
 
-export { companyLogo, companyLogo2, googlePlay, appStore };
+export {
+  companyLogo,
+  companyLogo2,
+  googlePlay,
+  appStore,
+  autoSlide1,
+  autoSlide2,
+  autoSlide3,
+};
