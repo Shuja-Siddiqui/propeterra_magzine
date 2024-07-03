20fd6a1d2a2cd8dae510ce7c1d2f9a9751e7f4fa
Shuja Ur Rehman <shuja0094@gmail.com>
2024-07-03 15:58:31 +0500

    new ui

---
+++ src/components/blogs/ArticleCardPair.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { CiClock1 } from "react-icons/ci";
+import { FaRegComment } from "react-icons/fa";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+function ArticleCardPair({ articles }) {
+  return (
+    <div className="border-t py-4">
+      <h2
+        className="flex items-center text-lg font-bold uppercase"
+        style={{
+          fontFamily: "Escrow Condensed, Alternate Times New Roman, serif",
+        }}
+      >
+        Health <MdKeyboardArrowRight size={24} />
+      </h2>
+      <div className="flex flex-col md:flex-row md:space-x-4">
+        {articles.map((article, index) => (
+          <div
+            key={index}
+            className={`flex-1 border-t md:border-t-0 md:border-l md:${
+              index === 0 ? "pl-0" : "pl-4"
+            } pt-4 md:pt-0`}
+          >
+            <p className="text-sm text-gray-500">{article.author}</p>
+            <h3
+              className="text-xl font-bold mt-2 transform scale-y-[1] md:scale-y-[1.8] lg:scale-y-[1.8] xl:scale-y-[1.8] 2xl:scale-y-[1.8] 3xl:scale-y-[1.8]"
+              style={{
+                fontFamily:
+                  "Escrow Condensed, Alternate Times New Roman, serif",
+              }}
+            >
+              {article.title}
+            </h3>
+            <p
+              className="mt-2 text-blog-description"
+              style={{ fontFamily: "Retina, Arial, Helvetica, sans-serif" }}
+            >
+              {article.description}
+            </p>
+            <div className="flex items-center mt-2 text-gray-500 text-sm gap-3">
+              <span className="flex items-center gap-1">
+                <FaRegComment />
+                {article.comments} comments
+              </span>
+              <span className="flex items-center gap-1">
+                <CiClock1 />
+                {article.readTime} min read
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ArticleCardPair;
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -7,6 +7,10 @@ import ArticleCardHorizontal from "./ArticleCardHorizontal";
 import { data, journalReports } from "../../api/dummy";
 import ArticleCardSimple from "./ArticleCardSimple";
 import JournalReports from "./JournalReports";
+import ArticleSummary from "./ArticleSummary";
+import ArticleTripeCards from "./ArticleTripleCards";
+import ArticleTripeHorizontal from "./ArticleTripleHorizontal";
+import ArticleCardPair from "./ArticleCardPair";
 
 function Blogs() {
   // Function to map data into sections
@@ -33,98 +37,125 @@ function Blogs() {
   return (
     <div className="max-w-screen-xl mx-auto xl:p-12 lg:p-12 md:p-12 sm:p-6 xs:p-6 xss:p-6">
       <div className="flex xl:flex-row lg:flex-row md:flex-row xss:flex-col 2xl:space-x-8 xl:space-x-8 lg:space-x-8 md:space-x-8 sm:space-x-0 xs:space-x-0 xss:space-x-0">
-        {/* Left Section */}
-        <div className="2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-1/4 sm:w-full xs:w-full xss:w-full space-y-8">
-          {leftSection?.map((article, index) => (
-            <ArticleCard
-              key={index}
-              title={article.title}
-              description={article?.full_text || article?.title}
-              comments={0}
-              readTime={5}
-              subArticles={[]}
-              article={article}
-            />
-          ))}
-        </div>
-
-        {/* Center Section */}
-        <div className="2xl:w-2/4 xl:w-2/4 lg:w-2/4 md:w-2/4 sm:w-full xs:w-full xss:w-full xl:space-y-8 xss:space-y-0">
-          {centerSection?.map((article, index) => (
-            <div key={index}>
-              {index === 0 ? (
-                <ArticleOne
-                  image={
-                    article.images[0] || "https://via.placeholder.com/800x400"
-                  }
+        {/* Left Parent */}
+        <div className="2xl:w-1/4 xl:w-3/4 lg:w-1/4 md:w-1/4 sm:w-full xs:w-full xss:w-full">
+          {/* Top */}
+          <div className="w-full flex justify-between xl:flex-row lg:flex-row md:flex-row xss:flex-col">
+            {/* Left Section */}
+            <div className="2xl:w-[26%] xl:w-[26%] lg:w-[26%] md:w-[26%]sm:w-full xs:w-full xss:w-full space-y-8">
+              {leftSection?.map((article, index) => (
+                <ArticleCard
+                  key={index}
                   title={article.title}
                   description={article?.full_text || article?.title}
+                  comments={0}
+                  readTime={5}
+                  subArticles={[]}
                   article={article}
                 />
-              ) : (
-                index % 2 === 0 && (
-                  <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <ArticleTwo
+              ))}
+            </div>
+
+            {/* Center Section */}
+            <div className="2xl:w-[70%] xl:w-[70%] lg:w-[70%] md:w-[70%] sm:w-full xs:w-full xss:w-full space-y-8">
+              {centerSection?.map((article, index) => (
+                <div key={index}>
+                  {index === 0 ? (
+                    <ArticleOne
                       image={
                         article.images[0] ||
-                        "https://via.placeholder.com/400x300"
+                        "https://via.placeholder.com/800x400"
                       }
                       title={article.title}
                       description={article?.full_text || article?.title}
-                      comments={0}
-                      readTime={5}
                       article={article}
                     />
-                    {centerSection[index + 1] && (
-                      <ArticleTwo
-                        image={
-                          centerSection[index + 1].images[0] ||
-                          "https://via.placeholder.com/400x300"
-                        }
-                        title={centerSection[index + 1]?.title}
-                        description={
-                          centerSection[index + 1]?.full_text ||
-                          centerSection[index + 1]?.title
-                        }
-                        comments={0}
-                        readTime={5}
-                        article={centerSection[index + 1]}
-                      />
-                    )}
-                  </div>
-                )
-              )}
-            </div>
-          ))}
+                  ) : (
+                    index % 2 === 0 && (
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                        <ArticleTwo
+                          image={
+                            article.images[0] ||
+                            "https://via.placeholder.com/400x300"
+                          }
+                          title={article.title}
+                          description={article?.full_text || article?.title}
+                          comments={0}
+                          readTime={5}
+                          article={article}
+                        />
+                        {centerSection[index + 1] && (
+                          <ArticleTwo
+                            image={
+                              centerSection[index + 1].images[0] ||
+                              "https://via.placeholder.com/400x300"
+                            }
+                            title={centerSection[index + 1]?.title}
+                            description={
+                              centerSection[index + 1]?.full_text ||
+                              centerSection[index + 1]?.title
+                            }
+                            comments={0}
+                            readTime={5}
+                            article={centerSection[index + 1]}
+                          />
+                        )}
+                      </div>
+                    )
+                  )}
+                </div>
+              ))}
 
-          {/* Horizontal Article */}
-          <div className="space-y-8">
-            {centerSection?.length > 1 && (
-              <ArticleCardHorizontal
-                image={
-                  centerSection[1].images[0] ||
-                  "https://via.placeholder.com/330x247"
-                }
-                category={centerSection[1]?.category?.toUpperCase()}
-                title={centerSection[1].title}
-                description={
-                  centerSection[1]?.full_text || centerSection[1]?.title
-                }
-                article={centerSection[1]}
-              />
-            )}
+              {/* Horizontal Article */}
+              <div className="space-y-8">
+                {centerSection?.length > 1 && (
+                  <ArticleCardHorizontal
+                    image={
+                      centerSection[1].images[0] ||
+                      "https://via.placeholder.com/330x247"
+                    }
+                    category={centerSection[1]?.category?.toUpperCase()}
+                    title={centerSection[1].title}
+                    description={
+                      centerSection[1]?.full_text || centerSection[1]?.title
+                    }
+                    article={centerSection[1]}
+                  />
+                )}
+              </div>
+
+              {/* Simple Cards */}
+              <div className="space-y-4">
+                {centerSection?.slice(2)?.map((article, index) => (
+                  <ArticleCardSimple
+                    key={index}
+                    title={article?.title}
+                    description={article?.full_text || article?.title}
+                    article={article}
+                  />
+                ))}
+              </div>
+            </div>
           </div>
 
-          {/* Simple Cards */}
-          <div className="space-y-4">
-            {centerSection?.slice(2)?.map((article, index) => (
-              <ArticleCardSimple
+          <div className="w-full">
+            {centerSection.slice(3).map((article, index) => (
+              <ArticleSummary
                 key={index}
-                title={article?.title}
-                description={article?.full_text || article?.title}
-                article={article}
+                image={
+                  article.images[0] || "https://via.placeholder.com/330x247"
+                }
+                title={article.title}
+                description={article.description}
+                link={article.link}
               />
             ))}
+
+            {/* <ArticleTripeCards /> */}
+
+            <ArticleTripeHorizontal />
+
+            <ArticleCardPair articles={centerSection.slice(0, 2)} />
           </div>
         </div>
 
